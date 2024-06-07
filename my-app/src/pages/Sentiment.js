28470379ad55bfb7a2c856407367376a30439d2b
Hayun Jung <60186469+Yun505@users.noreply.github.com>
2024-06-07 15:29:12 -0400

    for future sentiment

---
+++ my-app/src/pages/Sentiment.js
@@ -0,0 +1,28 @@
+import sentiment from "../images/Sentiment.png";
+
+
+export default function Sentiment() {
+  return (
+    <div className="container pl-20 pr-20">
+      <br></br>
+      <br></br>
+      <br></br>
+      <h1 className="text-5xl text-sky-500">Sentiment Analysis</h1>
+
+      <br className="space-y-8"></br>
+      <br className="space-y-8"></br>
+      <article className="content text-md">
+        {/* <h3 className="text-3xl">Idea:</h3>
+        <br className="space-y-8"></br>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/xOyyNY_SQwc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        <br className="space-y-8"></br>
+        <br className="space-y-3"></br> */}
+       
+
+        {/* <img src={} className="inline-photo"></img> */}
+      </article>
+
+
+    </div>
+  )
+}
